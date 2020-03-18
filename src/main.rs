89eeb6de1a570512131ae52e7f686bf69dd344cf
tsufeki <tsufeki@ymail.com>
2020-03-18 16:19:50 +0100

    Use abbr instead of word

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use fuzzy_matcher::skim::fuzzy_match;
-use serde::{Deserialize, Serialize};
-use serde_json::{from_str, to_writer, Value};
 use std::collections::HashMap;
 use std::io::{self, BufRead, Write};
 use std::iter::repeat;
 use std::vec::Vec;
 
-#[derive(Serialize, Deserialize, Debug)]
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_writer, Value};
+
+#[derive(Serialize, Deserialize)]
 struct CompletionItem {
     word: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    abbr: Option<String>,
 
     #[serde(flatten)]
     rest: HashMap<String, Value>,
@@ -20,25 +24,39 @@ struct Ranked<'a, T> {
     rank: i64,
 }
 
-fn rank_item<'a>(
-    item: &'a CompletionItem,
-    pattern: &str,
-    priority: i64,
-) -> Option<Ranked<'a, CompletionItem>> {
-    fuzzy_match(&item.word, &pattern).map(|rank| Ranked {
-        item,
-        rank,
-        priority,
-    })
+struct Ranker<'a, M: FuzzyMatcher> {
+    pattern: &'a str,
+    matcher: M,
 }
 
-#[derive(Deserialize, Debug)]
+impl<'a, M: FuzzyMatcher> Ranker<'a, M> {
+    pub fn new(pattern: &'a str, matcher: M) -> Self {
+        Ranker { pattern, matcher }
+    }
+
+    pub fn rank_item<'b>(
+        &self,
+        item: &'b CompletionItem,
+        priority: i64,
+    ) -> Option<Ranked<'b, CompletionItem>> {
+        let text = item.abbr.as_deref().unwrap_or(&item.word);
+        self.matcher
+            .fuzzy_match(text, self.pattern)
+            .map(|rank| Ranked {
+                item,
+                rank,
+                priority,
+            })
+    }
+}
+
+#[derive(Deserialize)]
 struct CompletionList {
     items: Vec<CompletionItem>,
     priority: i64,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct Completions {
     pattern: String,
     lists: Vec<CompletionList>,
@@ -46,12 +64,14 @@ struct Completions {
 
 impl Completions {
     pub fn filter(&self) -> Vec<&CompletionItem> {
+        let ranker = Ranker::new(&self.pattern, SkimMatcherV2::default());
+
         let mut ranked: Vec<Ranked<CompletionItem>> = self
             .lists
             .iter()
             .map(|list| (list.items.iter(), repeat(list.priority)))
             .flat_map(|(items, priorities)| items.zip(priorities))
-            .filter_map(|(item, priority)| rank_item(&item, &self.pattern, priority))
+            .filter_map(|(item, priority)| ranker.rank_item(item, priority))
             .collect();
 
         ranked.sort_unstable_by_key(|item| (-item.priority, -item.rank));
